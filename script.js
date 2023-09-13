3ca80e119497b108656e2fdff8e289aa9562a68e
tomaspa-dev <blord.dev@gmail.com>
2023-09-13 15:11:20 -0500

    Solving Challenge 2
    Solving challenge 2

--- script.js
+++ script.js
@@ -54,7 +54,143 @@ const restaurant = {
   },
 };
 
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+//
+// 1.Loop over the game.scored array and print each player name to the console,
+// along with the goal number (Example: "Goal 1: Lewandowski")
+const entries = Object.entries(game.scored);
+// console.log(entries);
+// console.log(game.scored.length);
+for (const [goal, player] of entries) {
+  console.log(`Goal${Number(goal) + 1}: ${player}`);
+}
+
+//2 Use a loop to calculate the average odd and log it to the console (We already
+// studied how to calculate averages, you can go check if you don't remember)
+
+const values = Object.values(game.odds);
+// console.log(values);
+
+let sum = 0;
+for (const i of values) {
+  sum += i;
+}
+let prom = sum / values.length;
+console.log(prom);
+
+//3.
+// Print the 3 odds to the console, but in a nice formatted way, exactly like this:
+// Odd of victory Bayern Munich: 1.33
+// Odd of draw: 3.25
+// Odd of victory Borrussia Dortmund: 6.5
+// Get the team names directly from the game object, don't hardcode them
+// (except for "draw"). Hint: Note how the odds and the game objects have the
+// same property names
+
+// console.log(`Odd of victory ${game.team1}: ${game.odds}`);
+const odds = Object.entries(game.odds);
+// console.log(odds);
+
+for (const [i, el] of odds) {
+  // console.log(i);
+  console.log(`Odd of victory ${i === 'x' ? 'Draw' : i}: ${el}`);
+}
+
+//Challenge 2 Start
+
+/* Coding Challenge #2
+Let's continue with our football betting app! Keep using the 'game' variable from
+before.
+Your tasks:
+1. Loop over the game.scored array and print each player name to the console,
+along with the goal number (Example: "Goal 1: Lewandowski")
+2. Use a loop to calculate the average odd and log it to the console (We already
+studied how to calculate averages, you can go check if you don't remember)
+3. Print the 3 odds to the console, but in a nice formatted way, exactly like this:
+Odd of victory Bayern Munich: 1.33
+Odd of draw: 3.25
+Odd of victory Borrussia Dortmund: 6.5
+Get the team names directly from the game object, don't hardcode them
+(except for "draw"). Hint: Note how the odds and the game objects have the
+same property names 😉
+4. Bonus: Create an object called 'scorers' which contains the names of the
+players who scored as properties, and the number of goals as the value. In this
+game, it will look like this:
+{
+Gnarby: 1,
+Hummels: 1,
+Lewandowski: 2
+} */
+
+//Challenge 2 Fin
+
+//Object keys
+//Properties NAMES
+/* 
+const properties = Object.keys(openingHours);
+console.log(properties);
+
+let openStr = `We are open on ${properties.length} days`;
+
+for (const day of properties) {
+  openStr += `${day},`;
+}
+
+//Properties VALUES
+const values = Object.values(openingHours);
+console.log(typeof values, values);
+
+//Entire object
+const entries = Object.entries(openingHours);
+console.log(entries);
+
+//[key, value]
+for (const [day, { open, close }] of entries) {
+  console.log(`On ${day} we open at ${open} and close at ${close}`);
+}
+ */
 // Optional Chaining ?.
+/* 
 console.log(restaurant.openingHours.fri);
 console.log(restaurant.openingHours.mon?.open);
 console.log(restaurant.openingHours.sat?.open);
@@ -76,7 +212,8 @@ const users = [{ name: 'Jonas', email: 'hello@jonas.io' }];
 console.log(users[0]?.name ?? 'User array is empty');
 
 if (users.length > 0) console.log(users[0]?.name);
-else console.log('user array is empty');
+else console.log('user array is empty'); 
+*/
 
 //Looping arrays For of, entries
 
