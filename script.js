1cfc77396cf8682014a874b1b507996c8be38184
tomaspa-dev <blord.dev@gmail.com>
2023-09-14 08:53:38 -0500

    Solving Coding Challenge #3
    using maps, arrays, ternary operator

--- script.js
+++ script.js
@@ -54,8 +54,78 @@ const restaurant = {
   },
 };
 
-//Maps//
+/////////////////////////////////////////////////////////////////////////////////
+// Coding Challenge #3//////////////////////////////////////////////////////////
+// Let's continue with our football betting app! This time, we have a map called
+// 'gameEvents' (see below) with a log of the events that happened during the
+// game. The values are the events themselves, and the keys are the minutes in which
+// each event happened (a football game has 90 minutes plus some extra time).
+// Your tasks:
+// 1. Create an array 'events' of the different game events that happened (no
+// duplicates)
+// 2. After the game has finished, is was found that the yellow card from minute 64
+// was unfair. So remove this event from the game events log.
+// 3. Compute and log the following string to the console: "An event happened, on
+// average, every 9 minutes" (keep in mind that a game has 90 minutes)
+// 4. Loop over 'gameEvents' and log each element to the console, marking
+// whether it's in the first half or second half (after 45 min) of the game, like this:
+// [FIRST HALF] 17:
+// ⚽
+// GOAL
 
+const gameEvents = new Map([
+  [17, '⚽ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽ GOAL'],
+  [80, '⚽ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+
+// const game = [...gameEvents.values()];
+// console.log(game);
+
+// 1. Create an array 'events' of the different game events that happened (no
+// duplicates)
+const events = new Set([...gameEvents.values()]);
+console.log(events);
+
+// 2. After the game has finished, is was found that the yellow card from minute 64
+// was unfair. So remove this event from the game events log.
+
+gameEvents.delete(64);
+console.log(gameEvents);
+
+// 3. Compute and log the following string to the console: "An event happened, on
+// average, every 9 minutes" (keep in mind that a game has 90 minutes)
+
+for (let min = 0; min <= 90; min += 9) {
+  console.log(`An event happened, on ${min}`);
+}
+
+// 4. Loop over 'gameEvents' and log each element to the console, marking
+// whether it's in the first half or second half (after 45 min) of the game, like this:
+// [FIRST HALF] 17: ⚽ GOAL
+
+const minutes = [...gameEvents.keys()];
+console.log(minutes);
+
+for (const min of minutes) {
+  min < 45
+    ? console.log(`[FIRST HALF] ${min} ${gameEvents.get(min)}`)
+    : console.log(`[SECOND HALF] ${min} ${gameEvents.get(min)}`);
+}
+
+// Coding Challenge #3///////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
+
+//Maps//
+/* 
 const question = new Map([
   ['question', 'What is the best programming language in the world?'],
   [1, 'C'],
@@ -91,6 +161,7 @@ console.log([...question]);
 console.log([...question.keys()]);
 console.log([...question.values()]);
 //
+ */
 
 //Fundamentals
 
