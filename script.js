57cefd08192a137d16c937ea486e9c0a09b92703
tomaspa-dev <blord.dev@gmail.com>
2023-09-13 19:27:24 -0500

    example with question
    question prompt and message with the answer

--- script.js
+++ script.js
@@ -56,9 +56,42 @@ const restaurant = {
 
 //Maps//
 
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct 🎉'],
+  [false, 'Try again'],
+]);
+
+console.log(question);
+
+console.log(Object.entries(openingHours));
+
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+}
+
+// const answer = Number(prompt('Your answer'));
+// console.log(answer);
+
+const answer = Number(prompt(question.get('question')));
+console.log(question.get(question.get('correct') === answer));
+
+// Convert map to array
+console.log(...question);
+
+//
+
 //Fundamentals
 
-const rest = new Map();
+/* const rest = new Map();
 rest.set('name', 'Classico Italiano');
 rest.set(1, 'Firenze Italy');
 rest.set(2, 'Lisbon, Portugal');
@@ -85,7 +118,7 @@ rest.set(arr, 'Test');
 rest.set(document.querySelector('h1'), 'Heading');
 console.log(rest);
 console.log(rest.size);
-console.log(rest.get(arr));
+console.log(rest.get(arr)); */
 
 //Maps//
 
